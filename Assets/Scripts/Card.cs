fbc6fd1458f9de9adef981110c403abc0e7041cd
yukgeunmu <36117247+yukgeunmu@users.noreply.github.com>
2025-01-21 19:40:34 +0900

    카드 뒤집기 수정 및 카드섞기

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    public int index;
+    public SpriteRenderer frontimage;
     public GameObject front;
     public GameObject back;
 
     public Animator anim;
+
     
     void Start()
     {
@@ -26,5 +29,12 @@ public class Card : MonoBehaviour
         front.SetActive(true);
         back.SetActive(false);
         
+    }
+
+    public void Setting(int number)
+    {
+        index = number;
+        frontimage.sprite = Resources.Load<Sprite>($"card{index}");
+
     }
 }
