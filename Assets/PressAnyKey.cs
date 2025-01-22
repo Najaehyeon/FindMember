ef36fbf54c35cf6ace4ff7c88fc677d85d69ba6a
Najaehyeon <jaehyeon4832@gmail.com>
2025-01-22 12:55:16 +0900

    텍스트 반짝반짝

--- Assets/PressAnyKey.cs
+++ Assets/PressAnyKey.cs
@@ -7,7 +7,8 @@ public class PressAnyKey : MonoBehaviour
 {
     Text text;
     float alpha = 0f; // 알파값을 조절하기 위한 변수
-    float speed = 1f; // 알파값 변화 속도
+    float speed = 3f; // 알파값 변화 속도
+    float customTime = 0f; // 클리어 씬에서만 사용되는 독립 시간
 
     void Start()
     {
@@ -17,8 +18,12 @@ public class PressAnyKey : MonoBehaviour
     
     void Update()
     {
-        // Mathf.PingPong을 사용해 알파값을 0~1 사이로 변동
-        alpha = Mathf.PingPong(Time.time * speed, 1f);
+        // Time.unscaledDeltaTime을 사용하여 독립 시간 계산
+        customTime += Time.unscaledDeltaTime * speed;
+
+        // 알파값 계산 (0~1 범위로 변동)
+        alpha = (Mathf.Sin(customTime) + 0.8f) / 2f;
+
 
         // 텍스트의 색상 업데이트
         Color color = text.color;
