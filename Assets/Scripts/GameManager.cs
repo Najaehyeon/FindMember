b302138a78cf658f72204c2d0edf2204ee91c607
KangHyunAh <fkslsn@gmail.com>
2025-01-22 11:25:38 +0900

    DeleteCard & ClearScene Change

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -10,6 +12,10 @@ public class GameManager : MonoBehaviour
     public GameObject endPanel;
     public Text timeTxt;
     float time;
+
+    public Card firstCard;
+    public Card secondCard;
+    public int cardCount;
 
     public void Awake()
     {
@@ -26,13 +32,45 @@ public class GameManager : MonoBehaviour
         
     }
 
+    public void isMatched()
+    {
+        if (firstCard.index == secondCard.index) 
+        {
+            checkMatched();
+            cardCount -= 2;
+            if (cardCount == 0) 
+            {
+                SceneManager.LoadScene("ClearScene");
+                Time.timeScale = 0.0f;
+            }
+        }
+        else 
+        {
+            checkMatched();
+        }
+    }
+    public void checkMatched() 
+        {
+            if (firstCard.index == secondCard.index) 
+            {
+                firstCard.DestroyCard();
+                secondCard.DestroyCard();
+            }
+            else 
+            { 
+                firstCard.CloseCard();
+                secondCard.CloseCard();
+            }
+            firstCard = null;
+            secondCard = null;
+        }
     // Update is called once per frame
     void Update()
     {
         
         time = float.Parse(timeTxt.text);
 
-        if(time > 0)
+        if (time > 0)
         {
             time -= Time.deltaTime;
             timeTxt.text = time.ToString("N2");
@@ -42,10 +80,9 @@ public class GameManager : MonoBehaviour
             endPanel.SetActive(true);
             Time.timeScale = 0.0f;
             timeTxt.text = "0.00";
-  
         }
-
-
-        
     }
+
+    
+        
 }
